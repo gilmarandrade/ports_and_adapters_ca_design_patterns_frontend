27c4001c50f86c1339650b0d99ddc750d9b94d47
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-29 14:49:21 -0300

    feat [frontend]: list todos and add todos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const todos = reactive([
+  { description: 'Estudar Typescript', done: true },
+  { description: 'Fazer a prova online', done: false },
+  { description: 'Cortar a grama', done: false },
+])
+
+const description = ref("")
+
+function addItem() {
+  todos.push({description: description.value, done: false})
+  description.value = ""
+}
 </script>
 
 <template>
-  <div>
-    hello
-    
+  <div v-for="item in todos">
+    <span v-bind:style="{ 'text-decoration': item.done ? 'line-through' : ''}">{{ item.description }}</span> {{ item.done }}
   </div>
-  
+  {{ description }}
+  <input type="text" v-model="description" @keyup.enter="addItem"/>
 </template>
 
 <style scoped>
-
-
 </style>
