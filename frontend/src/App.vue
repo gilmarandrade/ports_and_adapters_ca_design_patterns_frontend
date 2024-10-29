5a89a21ebd1793cf5c72f8f8d27d9d3319d756f8
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-29 15:15:38 -0300

    feat [frontend]: add todo, remove todo, toggle done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,15 +10,35 @@ const todos = reactive([
 const description = ref("")
 
 function addItem() {
-  todos.push({description: description.value, done: false})
+  const trimedDescription = description.value.trim()
+  if(!trimedDescription) return
+
+  if(todos.some((item: any) => item.description == trimedDescription)) return
+
+  if(todos.filter((item: any) => !item.done).length > 4) return
+
+  todos.push({description: trimedDescription, done: false})
   description.value = ""
+}
+
+function removeItem(item: any) {
+  todos.splice(todos.indexOf(item), 1)
+}
+
+function toggleDone(item: any) {
+  item.done = !item.done
 }
 </script>
 
 <template>
+  <div v-if="todos.length == 0">No item</div>
   <div v-for="item in todos">
     <span v-bind:style="{ 'text-decoration': item.done ? 'line-through' : ''}">{{ item.description }}</span> {{ item.done }}
+
+    <button @click="removeItem(item)">remove</button>
+    <button @click="toggleDone(item)">done/undone</button>
   </div>
+  <hr />
   {{ description }}
   <input type="text" v-model="description" @keyup.enter="addItem"/>
 </template>
