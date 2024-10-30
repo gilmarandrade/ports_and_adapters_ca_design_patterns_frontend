b441ca9b1ec75b4a56573a47d3f6b996c09236dd
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-10-30 17:06:35 -0300

    refactor [frontend]: created TodoList entity

--- frontend/src/components/TodoListView.vue
+++ frontend/src/components/TodoListView.vue
@@ -1,70 +1,40 @@
 <script setup lang="ts">
 import { computed, inject, onMounted, reactive, ref } from 'vue';
 import type TodoGateway from '../gateway/TodoGateway';
+import TodoList from '../entities/TodoList';
+
+// await todoGateway.addItem(item)
+// await todoGateway.removeItem(item.id)
+// await todoGateway.updateItem(item)
 
 const data = reactive<any>({ 
-  todos: []
+  todoList: new TodoList()
 })
 
 const description = ref("")
 
 const todoGateway = inject('todoGateway') as TodoGateway 
 
-async function addItem() {
-  const trimedDescription = description.value.trim()
-  if(!trimedDescription) return
 
-  if(data.todos.some((item: any) => item.description == trimedDescription)) return
 
-  if(data.todos.filter((item: any) => !item.done).length > 4) return
-
-  const item = { id: Math.random().toString(36).slice(2, 7), description: trimedDescription, done: false}
-  data.todos.push(item)
-
-  description.value = ""
-
-  await todoGateway.addItem(item)
-}
-
-async function removeItem(item: any) {
-  data.todos.splice(data.todos.indexOf(item), 1)
-
-  await todoGateway.removeItem(item.id)
-}
-
-async function toggleDone(item: any) {
-  item.done = !item.done
-
-  await todoGateway.updateItem(item)
-
-}
-
-const completed = computed(() => {
-  const total = data.todos.length
-  if(total == 0) return 0
-  const done = data.todos.filter((item: any) => item.done).length
-
-  return Math.round((done/total) * 100)
-})
-
-onMounted(async () => {
-  data.todos = await todoGateway.getTodos()
-})
+// onMounted(async () => {
+//   data.todos = await todoGateway.getTodos()
+// })
 </script>
 
 <template>
-  <div v-if="data.todos.length == 0">No item</div>
-  <span class="completed">{{ completed }}%</span>
-  <div v-for="item in data.todos">
+  <div v-if="data.todoList.items.length == 0">No item</div>
+  <span class="completed">{{ data.todoList.getCompleted() }}%</span>
+  <div v-for="item in data.todoList.items">
     {{ item.id }}
     <span v-bind:style="{ 'text-decoration': item.done ? 'line-through' : ''}">{{ item.description }}</span> {{ item.done }}
 
-    <button @click="removeItem(item)">remove</button>
-    <button @click="toggleDone(item)">done/undone</button>
+    <button @click="data.todoList.toggleDone(item)">done/undone</button>
+    <button @click="data.todoList.removeItem(item)">remove</button>
   </div>
   <hr />
   {{ description }}
-  <input type="text" v-model="description" @keyup.enter="addItem"/>
+  <input type="text" v-model="description" @keyup.enter="data.todoList.addItem(description)"/>
 </template>
 
 <style scoped>
---
+++ frontend/src/entities/TodoList.ts
@@ -0,0 +1,48 @@
+export default class TodoList {
+    items: any;
+
+    constructor() {
+        this.items = []
+    }
+
+    async addItem(description: string) {
+        const trimedDescription = description.trim();
+        if (!trimedDescription) return;
+
+        if (
+            this.items.some(
+                (item: any) => item.description == trimedDescription
+            )
+        )
+            return;
+
+        if (this.items.filter((item: any) => !item.done).length > 4) return;
+
+        const item = {
+            id: Math.random().toString(36).slice(2, 7),
+            description: trimedDescription,
+            done: false,
+        };
+        this.items.push(item);
+    }
+
+    async removeItem(item: any) {
+        this.items.splice(this.items.indexOf(item), 1);
+    }
+
+    async toggleDone(item: any) {
+        item.done = !item.done;
+    }
+
+    getItem(description: string) {
+        return this.items.find((item: any) => item.description == description)
+    }
+
+    getCompleted() {
+        const total = this.items.length;
+        if (total == 0) return 0;
+        const done = this.items.filter((item: any) => item.done).length;
+
+        return Math.round((done / total) * 100);
+    }
+}
